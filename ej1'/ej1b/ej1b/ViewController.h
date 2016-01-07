1f42ca4e4655a54418a9fd7f69760d6b6b1deb1f
dtilve <dananeyra@Woloxs-Mac-mini-3.local>
2016-01-07 18:13:04 -0300

    final

---
+++ ej1'/ej1b/ej1b/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ej1b
+//
+//  Created by Dana Neyra on 1/7/16.
+//  Copyright © 2016 Dana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ejb/ej1b/ej1b/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ej1b
+//
+//  Created by Dana Neyra on 1/7/16.
+//  Copyright © 2016 Dana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
